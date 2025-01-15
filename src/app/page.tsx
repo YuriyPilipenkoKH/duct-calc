85deb186d477a584c5588e10f126dc109015d28f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-15 23:08:36 +0200

    lib & utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <div>homapage</div>
+    <div>
+      <h1>homapage</h1>
+      <Link href={"/login"}>login</Link>
+    </div>
   );
 }
