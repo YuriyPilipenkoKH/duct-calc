997582d765b6bb3eeda709d3dd245830f444a027
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-15 21:36:55 +0200

    Add GitHub provider to NextAuth and implement middleware for authentication

--- auth.ts
+++ auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github"
 import { prisma } from "./prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [Google, GitHub],
  })
