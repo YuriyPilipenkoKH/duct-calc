798c25106314199d467a8845513e8faa3b153a89
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-15 22:34:40 +0200

    successful auth

--- auth.ts
+++ auth.ts
@@ -1,11 +1,20 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github"
-import { prisma } from "./prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [Google, GitHub],
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
  })
